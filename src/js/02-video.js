3da122a7521d9b15b198d1dc657e81e9bf3a3a34
Nazar Voitekh <nazar.voitekh@gmail.com>
2023-02-01 15:00:19 +0200

    bug fixed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,20 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
-const iframeElem = document.querySelector('iframe');
-const iframePlayer = new Vimeo.Player(iframe);
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+//
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+  localStorage.setItem(STORAGE_KEY, seconds);
+}
+
+const currentTime = localStorage.getItem(STORAGE_KEY)
+  ? localStorage.getItem(STORAGE_KEY)
+  : 0;
+
+player.setCurrentTime(currentTime);
