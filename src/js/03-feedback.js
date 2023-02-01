e702c4d0a55896f41d7e4b930634f977f288b3c3
Nazar Voitekh <nazar.voitekh@gmail.com>
2023-02-01 16:30:36 +0200

    add third task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
-console.log('hi');
+import throttle from 'lodash.throttle';
+const STORAGE_KEY = 'feedback-form-state';
+
+const formEl = document.querySelector('.feedback-form');
+const email = document.querySelector('input');
+const message = document.querySelector('textarea');
+
+let formData = {};
+
+formEl.addEventListener('submit', onSubmitForm);
+formEl.addEventListener('input', throttle(onFormData, 500));
+
+listenTextarea();
+
+function onSubmitForm(event) {
+  event.preventDefault();
+
+  const { email, message } = event.currentTarget;
+  const formInput = {
+    email: email.value,
+    message: message.value,
+  };
+  console.log(formInput);
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onFormData(event) {
+  formData[event.target.name] = event.target.value;
+  localStorage.getItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function listenTextarea() {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+
+  if (savedMessage) {
+    formData = JSON.parse(savedMessage);
+    email.value = formData.email;
+    message.value = formData.message;
+  }
+}
